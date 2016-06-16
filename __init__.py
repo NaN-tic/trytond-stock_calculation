530dca99503ba0f4a7b5b9bd8e15449600405d73
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-06-16 15:32:45 +0200

    Initial release

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import Pool
+from .stock import *
+
+
+def register():
+    Pool.register(
+        module='stock_calculation', type_='model')
