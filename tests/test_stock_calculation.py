cc271241e7167df4cbb972e64e68b1a8edd75172
Raimon Esteve <raimon@nan-tic.com>
2016-06-23 11:54:25 +0200

    Improve coding style

--- tests/test_stock_calculation.py
+++ tests/test_stock_calculation.py
@@ -1,15 +1,13 @@
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+#!/usr/bin/env python
+#This file is part of Tryton.  The COPYRIGHT file at the top level of
+#this repository contains the full copyright notices and license terms.
 import unittest
-# import doctest
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_view, test_depends
-# TODO: Remove if no sceneario needed.
-# from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
-class TestCase(unittest.TestCase):
-    'Test module'
+class StockCalculationTestCase(unittest.TestCase):
+    'Test Stock Calculation module'
 
     def setUp(self):
         trytond.tests.test_tryton.install_module('stock_calculation')
@@ -21,9 +19,6 @@ class TestCase(unittest.TestCase):
 
 def suite():
     suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(TestCase))
-    # TODO: remove if no scenario needed.
-    #suite.addTests(doctest.DocFileSuite('scenario_stock_calculation.rst',
-    #        setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-    #        optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
+    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
+        StockCalculationTestCase))
     return suite
