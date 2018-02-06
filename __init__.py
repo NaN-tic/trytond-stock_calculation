bf6d711581e593bbab928eb6b50865cc22d7170c
Raimon Esteve <raimon@nan-tic.com>
2018-02-06 14:06:58 +0100

    Not necesary register classes

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
-from trytond.pool import Pool
-from . import stock
-
-
-def register():
-    Pool.register(
-        module='stock_calculation', type_='model')
