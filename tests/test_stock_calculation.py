8287b2eace9592c7c36379062c96345a8db7b526
?ngel ?lvarez <angel@nan-tic.com>
2018-04-27 09:28:39 +0200

    update to 4.7

--- tests/test_stock_calculation.py
+++ tests/test_stock_calculation.py
@@ -1,20 +1,14 @@
 #!/usr/bin/env python
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+# The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class StockCalculationTestCase(unittest.TestCase):
+class StockCalculationTestCase(ModuleTestCase):
     'Test Stock Calculation module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('stock_calculation')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'stock_calculation'
 
 
 def suite():
